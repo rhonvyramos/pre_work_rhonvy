592a39f1b380efb50dfb1a69cafa2d38e1f8c8f9
rhonvyramos <rhonvyramos@gmail.com>
2023-03-14 09:27:32 -0400

    added JavaScript to project

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// var topic = "JavaScript";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var t = 0; t < topics.length; t++) {
+    console.log(topics[t]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log("Please try again!");
+}
+*/
+
+/*
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var s = 0; s < shapes.length; s++) {
+    console.log(shapes[s]);
+}
+*/
